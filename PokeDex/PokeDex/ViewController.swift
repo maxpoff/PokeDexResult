9cbe67d865b8bca971f9c3227371d50b5d542cd7
Maxwell Poffenbarger <maxwellpoffenbarger@Maxwells-MacBook-Pro.local>
2020-01-21 10:16:11 -0700

    Created the project

---
+++ PokeDex/PokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Maxwell Poffenbarger on 1/21/20.
+//  Copyright © 2020 Poff Daddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
